d206fd7e597a032ea19184ba1b1ed0dce3509006
Ponus Cheung <zhangsopor@gmail.com>
2021-06-30 17:33:08 +0800

    update

--- manager/controllers/dbForwardController.go
+++ manager/controllers/dbForwardController.go
@@ -4,6 +4,7 @@ import (
 	"dbaf/manager/models"
 	"dbaf/manager/services"
 	"github.com/gin-gonic/gin"
+	uuid "github.com/satori/go.uuid"
 	"strconv"
 )
 
@@ -15,9 +16,7 @@ func AddDbForward(c *gin.Context) {
 	dbType := c.PostForm("type")
 
 	lport, _ := strconv.Atoi(listenPort)
-
 	dport, _ := strconv.Atoi(dbPort)
-
 	t, _ := strconv.Atoi(dbType)
 
 	tmp := &models.DbForward{Name: name, ListenPort: lport, DbIp: dbIp, DbPort: dport, Type: t}
@@ -34,8 +33,18 @@ func UpdateDbForward(c *gin.Context) {
 }
 
 func DeleteDbForward(c *gin.Context) {
-
-	c.JSON(200, &Ret{CODE_FALURE, false, "", nil})
+	id := c.Query("id")
+	uuid, err := uuid.FromString(id)
+	if err != nil {
+		c.JSON(200, Ret{CODE_PARAM_ERROR, true, MSG_PARAM_ERROR, nil})
+	} else {
+		success := services.DeleteForward(uuid)
+		if success {
+			c.JSON(200, Ret{CODE_SUCCESS, true, MSG_SUCCESS, nil})
+		} else {
+			c.JSON(200, Ret{CODE_FALURE, false, MSG_FAILURE, nil})
+		}
+	}
 }
 
 func FindDbForward(c *gin.Context) {
